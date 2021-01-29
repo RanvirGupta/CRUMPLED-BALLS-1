297f55da5a961bc1a3b26ca526fe216ba3bc34e9
RanvirGupta <66317523+RanvirGupta@users.noreply.github.com>
2021-01-29 18:26:25 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,23 @@
+class Paper{
+    constructor(x, y, r){
+        var options = {
+            restitution: 0.6,
+            friction: 0.5,
+            density: 1.2,
+        }
+        this.body = Bodies.circle(x, y, r ,options);
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        World.add(world, this.body);
+    }
+    display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x, pos.y);
+        ellipseMode(RADIUS);
+        ellipse(0,0, this.r, this.r);
+        pop();
+        fill("yellow");
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,56 @@
+var paper,ground;
+var dustbin;
+var left, right, bottom;
+var ground;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload(){
+	
+}
+
+function setup() {
+	createCanvas(1500,400);
+	
+
+	engine = Engine.create();
+	world = engine.world;
+    var options = {
+	isStatic: true
+}
+	ground = Bodies.rectangle(750, 390,2000,20, options);
+	World.add(world, ground);
+
+	paper = new Paper(200,300,20);
+	
+
+	dustbinSideB = new Dustbin(1150,373,200,15);
+	dustbinSideL = new Dustbin(1050,306,15,150);
+	dustbinSideR = new Dustbin(1250,306,15,150);
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+   background(0);
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y , 1500,20);
+  dustbinSideB.display()
+  dustbinSideL.display()
+  dustbinSideR.display()
+  paper.display();
+  
+}
+
+function keyPressed(){
+	if (keyCode === UP_ARROW){
+		Matter.Body.applyForce(paper.body,paper.body.position,{x:85,y:-85});
+	}
+}
+
